a05ce82c4e2414a7ebde40300c21897a86a67bd5
wobY <24554109+wobYY@users.noreply.github.com>
2025-04-09 23:25:19 +0200

    Added: get_spreadsheet method for the wrapper

--- de_toolkit/gsheets/de_gspread.py
+++ de_toolkit/gsheets/de_gspread.py
@@ -1,22 +1,33 @@
-"""Google Sheets Wrapper."""
+"""gspread Wrapper."""
 
 import logging
+import random
 
 import gspread
 import vcr
 from icecream import ic
+from tenacity import retry, stop_after_attempt
+from tenacity.retry import retry_if_not_exception_type
+from tenacity.wait import wait_exponential
 
 ic.configureOutput(includeContext=True)
 
+# ic.disable()
+
+DEFAULT_CREDENTIAL_SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
+]
+
 
 class DtkGspread:
-    """gspread wrapper."""
+    """gspread Wrapper."""
 
     def __init__(
         self,
         credentials: str | list[str] | list[dict],
         credentials_type: str = None,
-        credential_scopes: list[str] = None,
+        credential_scopes: list[str] = DEFAULT_CREDENTIAL_SCOPES,
         oauth_credentials_filename: str = None,
         oauth_authorized_user_filename: str = None,
     ) -> None:
@@ -31,9 +42,12 @@ class DtkGspread:
         """
         # We store logged-in clients in a list so that we can use
         # multiple accounts which helps with rate limiting trumendously
-        self.logged_in_clients = []
+        self.logged_in_clients: list[gspread.Client] = []
 
-        logging.debug(ic(credentials_type))
+        # Set the selected credential to None
+        self.__selected_credential: int | None = None
+
+        logging.debug("Selected credential type: %s", ic(credentials_type))
         if credentials_type == "oauth":
             # Login with the OAuth credentials
             oauth_gc, oauth_authorized_user = gspread.oauth(
@@ -46,13 +60,16 @@ class DtkGspread:
             # Login with the service accounts
             self._retrieve_service_accounts(credentials, credential_scopes)
 
+        # Set the credentials cache
+        self.__credentials_cache: list[gspread.Client] = self.logged_in_clients.copy()
+
     def _retrieve_service_accounts(
         self, credentials: str | list[str], credential_scopes: list[str]
     ) -> None:
         ic(credentials)
         if not isinstance(credentials, list):
             credentials = [credentials]
-        logging.debug(ic(len(credentials)))
+        logging.debug("Number of credentials provided: %s", ic(len(credentials)))
 
         # Go through each credential, login with each one and add it
         # to the list of logged in clients
@@ -73,3 +90,72 @@ class DtkGspread:
                 continue
 
             raise ValueError("Invalid credential!")
+
+    @property
+    def __credential(self) -> gspread.Client:
+        """Retrieve a random credential from the list of logged in clients."""
+        # Check if cache is empty
+        if ic(len(self.__credentials_cache)) == 0:
+            logging.debug(ic("Cache is empty, repopulating with logged in clients"))
+            self.__credentials_cache = self.logged_in_clients.copy()
+
+        # Get a random index
+        self.__selected_credential = random.randint(0, len(self.__credentials_cache) - 1)
+
+        # Get the selected credential
+        selected_credential = self.__credentials_cache[self.__selected_credential]
+
+        # Remove the selected credential from the __credentials_cache
+        self.__credentials_cache.pop(self.__selected_credential)
+
+        return selected_credential
+
+    @retry(
+        wait=wait_exponential(multiplier=2, min=2, max=30),
+        stop=stop_after_attempt(5),
+        reraise=True,
+        retry=retry_if_not_exception_type(ValueError),  # Only if it's gspread errors
+    )
+    def __get_spreadsheet(
+        self, spreadsheet_id: str = None, spreadsheet_name: str = None, spreadsheet_url: str = None
+    ) -> gspread.Spreadsheet:
+        """Get a spreadsheet by ID, name, or URL.
+
+        Args:
+            spreadsheet_id: The ID of the spreadsheet.
+            spreadsheet_name: The name of the spreadsheet.
+            spreadsheet_url: The URL of the spreadsheet.
+
+        Returns:
+            The spreadsheet object.
+        """
+        # If spreadsheet_id is provided, use it
+        if spreadsheet_id:
+            logging.debug("Spreadsheet ID provided: %s", ic(spreadsheet_id))
+            return self.__credential.open_by_key(spreadsheet_id)
+
+        # If spreadsheet_name is provided, use it
+        if spreadsheet_name:
+            logging.debug("Spreadsheet name provided: %s", ic(spreadsheet_name))
+            return self.__credential.open_by_url(spreadsheet_name)
+
+        # If spreadsheet_url is provided, use it
+        if spreadsheet_url:
+            logging.debug("Spreadsheet URL provided: %s", ic(spreadsheet_url))
+            return self.__credential.open_by_url(spreadsheet_url)
+
+        # If none of the above are provided, raise an error
+        raise ValueError("No spreadsheet ID, name, or URL provided.")
+
+    def get_spreadsheet(self, *args, **kwargs) -> gspread.Spreadsheet:
+        """Get a spreadsheet by ID, name, or URL.
+
+        Args:
+            spreadsheet_id: The ID of the spreadsheet.
+            spreadsheet_name: The name of the spreadsheet.
+            spreadsheet_url: The URL of the spreadsheet.
+
+        Returns:
+            The spreadsheet object.
+        """
+        return self.__get_spreadsheet(*args, **kwargs)
--- tests/de_spread_test.py
+++ tests/de_spread_test.py
@@ -3,6 +3,12 @@
 import pytest
 
 from de_toolkit.gsheets.de_gspread import DtkGspread
+from dotenv import load_dotenv
+import os
+import gspread
+
+# Load environment variables
+load_dotenv()
 
 
 def test_service_acc_invalid_credentials() -> None:
@@ -15,3 +21,27 @@ def test_oauth_no_credentials() -> None:
     """Test that the DtkGspread class raises an error when given no credentials for OAuth."""
     with pytest.raises(TypeError):
         DtkGspread(credentials="", credentials_type="oauth")
+
+
+def test_invalid_spreadsheet_id() -> None:
+    """Test that the DtkGspread class raises an error when given an invalid spreadsheet ID."""
+    with pytest.raises(
+        gspread.SpreadsheetNotFound, match="No spreadsheet ID, name, or URL provided."
+    ):
+        DtkGspread(credentials=os.environ["GSPREAD_CREDENTAILS"], spreadsheet_id="")
+
+
+def test_invalid_spreadsheet_name() -> None:
+    """Test that the DtkGspread class raises an error when given an invalid spreadsheet name."""
+    with pytest.raises(
+        gspread.NoValidUrlKeyFound, match="No spreadsheet ID, name, or URL provided."
+    ):
+        DtkGspread(credentials=os.environ["GSPREAD_CREDENTAILS"], spreadsheet_name="")
+
+
+def test_invalid_spreadsheet_url() -> None:
+    """Test that the DtkGspread class raises an error when given an invalid spreadsheet URL."""
+    with pytest.raises(
+        gspread.NoValidUrlKeyFound, match="No spreadsheet ID, name, or URL provided."
+    ):
+        DtkGspread(credentials=os.environ["GSPREAD_CREDENTAILS"], spreadsheet_url="")
